a016047d80a12875cfd7540cb18eb1dd1911f760
Long <vanlong5436@gmail.com>
2025-06-18 17:35:31 +0700

    scoop check package installed

--- bin/scoop.sh
+++ bin/scoop.sh
@@ -2,12 +2,20 @@
 
 scoop_install() {
     for package in "$@"; do
-        scoop install $package
+        if [ -d "$SCOOP_APPS/$package" ]; then
+            log DEBUG "$package installed"
+        else
+            scoop install $package
+        fi
     done
 }
 
 scoop_install_admin() {
     for package in "$@"; do
-        gsudo scoop install $package
+        if [ -d "$SCOOP_APPS/$package" ]; then
+            log DEBUG "$package installed"
+        else
+            gsudo scoop install $package
+        fi
     done
 }
