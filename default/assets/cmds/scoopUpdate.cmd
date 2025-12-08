b4e8f681bae11d42107b59c8c1a163d5342a70cc
mim <vanlong5436@gmail.com>
2025-12-08 21:30:17 +0700

    update scoopUpdate.cmd

--- default/assets/cmds/scoopUpdate.cmd
+++ default/assets/cmds/scoopUpdate.cmd
@@ -1,6 +1,12 @@
-(
-sudo scoop update * 
-scoop cleanup *
-scoop cache rm * 
-exit /b 0
+@echo off
+
+for /f "tokens=2 delims=," %%a in ('tasklist /fi "imagename eq WindowsTerminal.exe" /fo csv /nh') do (
+    set PID=%%~a
 )
+
+if defined PID (
+    taskkill /F /PID %PID%
+)
+
+start "" cmd /c "sudo scoop update * & scoop cleanup * & scoop cache rm *"
+exit
