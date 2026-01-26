fa982759c8661e2363b0508c91322550925da955
mim <vanlong5436@gmail.com>
2026-01-26 20:50:49 +0700

    fix bug shrink wsl

--- default/assets/cmds/shrink_wsl.cmd
+++ default/assets/cmds/shrink_wsl.cmd
@@ -1,5 +1,7 @@
 @echo off
-REM
+setlocal enabledelayedexpansion
+
+REM 
 for /d %%D in ("%LOCALAPPDATA%\Packages\CanonicalGroupLimited.*") do (
     if exist "%%D\LocalState\ext4.vhdx" (
         set "VHDX_PATH=%%D\LocalState\ext4.vhdx"
@@ -9,17 +11,20 @@ for /d %%D in ("%LOCALAPPDATA%\Packages\CanonicalGroupLimited.*") do (
 
 :found
 if not defined VHDX_PATH (
-    echo ext4.vhdx not found
+    echo ext4.vhdx not found.
     pause
     exit /b
 )
 
-REM 
-echo select vdisk file="%VHDX_PATH%" > "%temp%\diskpart_script.txt"
-echo compact vdisk >> "%temp%\diskpart_script.txt"
+echo %VHDX_PATH%
+
+REM
+(
+echo select vdisk file="%VHDX_PATH%"
+echo compact vdisk
+) > "%temp%\diskpart_script.txt"
 
 diskpart /s "%temp%\diskpart_script.txt"
-
 del "%temp%\diskpart_script.txt"
 
 echo Disk compacting complete.
