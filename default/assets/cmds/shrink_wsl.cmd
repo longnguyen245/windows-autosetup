91976fecc29b38f062880b3680c49e0fdff5290b
Long <vanlong5436@gmail.com>
2026-02-28 17:48:57 +0700

    update shrink wsl
    Signed-off-by: Long <vanlong5436@gmail.com>

--- default/assets/cmds/shrink_wsl.cmd
+++ default/assets/cmds/shrink_wsl.cmd
@@ -1,24 +1,40 @@
 @echo off
 setlocal enabledelayedexpansion
 
-REM 
+set "FOUND=0"
+
+REM --- Scan Store Ubuntu ---
 for /d %%D in ("%LOCALAPPDATA%\Packages\CanonicalGroupLimited.*") do (
     if exist "%%D\LocalState\ext4.vhdx" (
         set "VHDX_PATH=%%D\LocalState\ext4.vhdx"
-        goto :found
+        call :compact
+        set "FOUND=1"
     )
 )
 
-:found
-if not defined VHDX_PATH (
+REM --- Scan %LOCALAPPDATA%\wsl\*\ext4.vhdx ---
+for /d %%D in ("%LOCALAPPDATA%\wsl\*") do (
+    if exist "%%D\ext4.vhdx" (
+        set "VHDX_PATH=%%D\ext4.vhdx"
+        call :compact
+        set "FOUND=1"
+    )
+)
+
+if "%FOUND%"=="0" (
     echo ext4.vhdx not found.
     pause
     exit /b
 )
 
+echo Disk compacting complete.
+pause
+exit /b
+
+REM ============================
+:compact
 echo %VHDX_PATH%
 
-REM
 (
 echo select vdisk file="%VHDX_PATH%"
 echo compact vdisk
@@ -27,5 +43,4 @@ echo compact vdisk
 diskpart /s "%temp%\diskpart_script.txt"
 del "%temp%\diskpart_script.txt"
 
-echo Disk compacting complete.
-pause
+exit /b
